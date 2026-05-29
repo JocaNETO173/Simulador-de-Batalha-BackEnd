6afa6f6e94b2deb1e13f5c9e7ace3746720a0354
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-05-29 08:20:41 -0300

    Add files via upload

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
 class Monstro {
-    constructor(nome, hp){
+    constructor(nome, hp) {
         this.nome = nome;
         this.hp = Number(hp);
     }
-    receberDano(quantidade){
-        this.hp = quantidade;
-        if(hp <= 0){
+    receberDano(quantidade) {
+        this.hp -= quantidade;
+        if (hp <= 0) {
             this.hp = 0;
             return `${this.nome} foi derrotado.`
         }
@@ -14,13 +14,23 @@ class Monstro {
 }
 
 let monstroBatalha;
-function iniciarJogo(){
+function iniciarJogo() {
     const nomeDigitado = document.getElementById('nomeMonstro').value;
     const hpDigitado = document.getElementById('hpMonstro').value;
     monstroBatalha = new Monstro(nomeDigitado, hpDigitado);
-    function atualizaTela(){
-        document.getElementById('painel-criacao').style.display = 'none';
-        document.getElementById('painel-combate').style.display = 'block';
-    }
+    if (monstroBatalha.nome != "") {
+        function atualizaTela() {
+            document.getElementById('painel-criacao').style.display = 'none';
+            document.getElementById('painel-combate').style.display = 'block';
+            document.getElementById('statusNome').innerText = monstroBatalha.nome;
+            document.getElementById('statusHp').innerText = `Vida Atual: ${monstroBatalha.hp}`;
+        }
 
+    }
+    atualizaTela()
+}
+function baterNoMonstro() {
+    const quantidadeDigitada = document.getElementById('danoInput').value;
+    monstroBatalha.receberDano(quantidadeDigitada)
+    atualizaTela()
 }
